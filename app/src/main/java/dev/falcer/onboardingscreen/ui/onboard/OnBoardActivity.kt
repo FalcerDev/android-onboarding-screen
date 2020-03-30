5908ad7ee0f92f610330d90db8076e4fb8d6e42d
Argana Phangquestian <arganaphangquestian@gmail.com>
2020-03-30 19:05:58 +0700

    Final

--- app/src/main/java/dev/falcer/onboardingscreen/ui/onboard/OnBoardActivity.kt
+++ app/src/main/java/dev/falcer/onboardingscreen/ui/onboard/OnBoardActivity.kt
@@ -1,13 +1,39 @@
 package dev.falcer.onboardingscreen.ui.onboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dev.falcer.onboardingscreen.R
+import dev.falcer.onboardingscreen.data.entity.OnBoard
+import kotlinx.android.synthetic.main.activity_on_board.*
 
 class OnBoardActivity : AppCompatActivity() {
+
+    private val adapter = OnBoardAdapter()
+    private val PAGER = arrayListOf(
+        OnBoard(
+            R.drawable.img_focus,
+            "Focus on your task",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ), OnBoard(
+            R.drawable.img_increase,
+            "Increase your profit",
+            "Duis nulla nibh, suscipit at rutrum sit amet, consequat vel lacus."
+        ), OnBoard(
+            R.drawable.img_relax,
+            "Relax doing your job",
+            "Suspendisse id ultrices orci. Mauris consectetur interdum interdum."
+        )
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_board)
+        init()
+    }
+
+    private fun init() {
+        adapter.setData(PAGER)
+        viewPager.adapter = adapter
+        dots_indicator.setViewPager2(viewPager)
     }
 }
---
+++ app/src/main/java/dev/falcer/onboardingscreen/ui/onboard/OnBoardAdapter.kt
@@ -0,0 +1,41 @@
+package dev.falcer.onboardingscreen.ui.onboard
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import dev.falcer.onboardingscreen.R
+import dev.falcer.onboardingscreen.data.entity.OnBoard
+
+class OnBoardAdapter : RecyclerView.Adapter<OnBoardAdapter.ViewHolder>() {
+    private lateinit var dataList: List<OnBoard>
+    fun setData(data: List<OnBoard>) {
+        dataList = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_on_board, parent, false))
+    }
+
+    override fun getItemCount() = dataList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataList[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val image = view.findViewById<ImageView>(R.id.card_image)
+        private val title = view.findViewById<TextView>(R.id.card_title)
+        private val description = view.findViewById<TextView>(R.id.card_description)
+        fun bind(data: OnBoard) {
+            Glide.with(itemView).load(data.image).fitCenter().into(image)
+            title.text = data.title
+            description.text = data.description
+        }
+    }
+
+}
