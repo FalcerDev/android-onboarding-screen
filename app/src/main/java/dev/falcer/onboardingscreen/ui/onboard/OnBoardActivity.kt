8f7fc673636d7c469dd4b8407581fb878866963c
Argana Phangquestian <arganaphangquestian@gmail.com>
2020-03-30 18:24:37 +0700

    Initial Commit :fire:

---
+++ app/src/main/java/dev/falcer/onboardingscreen/ui/onboard/OnBoardActivity.kt
@@ -0,0 +1,13 @@
+package dev.falcer.onboardingscreen.ui.onboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.falcer.onboardingscreen.R
+
+class OnBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+    }
+}
